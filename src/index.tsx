8ed14d7208fceba0aa29336cb9a49b4a51430d9a
Brant Gurganus <brant@gurganus.name>
2022-06-12 22:56:16 -0400

    add service worker mechanisms

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { render } from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TermsOfService from './features/terms-of-service/TermsOfService';
 import PrivacyPolicy from './features/privacy-policy/PrivacyPolicy';
@@ -23,6 +24,11 @@ render(
     </Provider>
   </React.StrictMode>
 , container);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
