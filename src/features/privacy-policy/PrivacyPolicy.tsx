b69a0555a10c62599199bedac6cf8c65590f3aa5
Brant Gurganus <brant@gurganus.name>
2022-06-18 00:37:29 -0400

    update some content

--- src/features/privacy-policy/PrivacyPolicy.tsx
+++ src/features/privacy-policy/PrivacyPolicy.tsx
@@ -2,6 +2,7 @@ function PrivacyPolicy() {
     return (
         <div>
             <h1>Privacy Policy coming soon.</h1>
+            <p>Analytics and other services are used to monitor usage and diagnose issues. Various Google services are used.</p>
         </div>
     );
 }
--- src/features/terms-of-service/TermsOfService.tsx
+++ src/features/terms-of-service/TermsOfService.tsx
@@ -2,6 +2,7 @@ function TermsOfService() {
     return (
         <div>
             <h1>Terms of Service coming soon.</h1>
+            <p>This app is very early development. There is no guarantee about anything. Use functionality at your own risk.</p>
         </div>
     )
 }
