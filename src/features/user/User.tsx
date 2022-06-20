6d3cb4f44927314a3212db495da0a8562fa32112
Brant Gurganus <brant@gurganus.name>
2022-06-19 21:44:45 -0400

    a bit more user management functionality

--- src/features/user/User.tsx
+++ src/features/user/User.tsx
@@ -1,17 +1,49 @@
-import { getAuth } from "@firebase/auth";
+import { EmailAuthProvider, getAuth } from "@firebase/auth";
 import Container from "@mui/material/Container";
 import { getApp } from "@firebase/app";
 import TextField from "@mui/material/TextField";
 import { Avatar } from "@mui/material";
 import { Md5 } from "md5-typescript";
+import { reauthenticateWithCredential, updateEmail } from "firebase/auth";
+import { useState } from "react";
 
 function User() {
-  const user = getAuth(getApp()).currentUser;
+  const [user, setUser] = useState(getAuth(getApp()).currentUser);
+  const [password, setPassword] = useState("");
+  const originalEmail = user?.email || "";
+  const changeEmail = (email: string) => {
+    if (user !== null) {
+      reauthenticateWithCredential(
+        user,
+        EmailAuthProvider.credential(originalEmail, password)
+      ).then(() => {
+        updateEmail(user, email).then(() => {
+          setUser(getAuth(getApp()).currentUser);
+        });
+      });
+    }
+  };
   return (
     <Container>
       <h1>User management</h1>
       <TextField label="Display Name" defaultValue={user?.displayName} />
-      <TextField label="Email" defaultValue={user?.email} />
+      <TextField
+        label="Email"
+        defaultValue={originalEmail}
+        type="email"
+        onBlur={(event) => {
+          changeEmail(event.target.value);
+        }}
+      />
+      <TextField
+        onBlur={(event) => {
+          setPassword(event.target.value);
+        }}
+        id="password"
+        label="Password"
+        type="password"
+        required
+      />
       <Avatar
         src={
           user?.photoURL || user?.email
