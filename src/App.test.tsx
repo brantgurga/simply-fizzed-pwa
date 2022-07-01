3dda5d3441001c1852ea0d1ea52d23b8333eaf25
Brant Gurganus <brant@gurganus.name>
2022-06-30 22:08:19 -0400

    make test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,29 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+import { FirebaseOptions, initializeApp } from "@firebase/app";
 
 jest.doMock("firebaseui");
 jest.doMock("react-firebaseui");
 
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyA_nQdMwa1kKXPjVtoSkZKlf2rxfctnRF8",
+  authDomain: "tokyo-hold-352402.firebaseapp.com",
+  projectId: "tokyo-hold-352402",
+  storageBucket: "tokyo-hold-352402.appspot.com",
+  messagingSenderId: "257128045567",
+  appId: "1:257128045567:web:436ef5204f643916bd25ca",
+  measurementId: "G-JM5LYE3H3J",
+};
+initializeApp(firebaseConfig);
+
 test("renders logo", () => {
   render(
     <Provider store={store}>
-      <App />
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
     </Provider>
   );
 
